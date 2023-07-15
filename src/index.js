92806353b7daa4d29c42fe521165eafad50bd643
Stefan Moravcik <stefan@virsas.com>
2023-07-15 13:37:57 +0200

    added form elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import VLogo from "./components/element/VLogo.vue";
+import VForm from "./components/element/VForm.vue";
+import VFormInput from "./components/element/VFormInput.vue";
+import VFormSelect from "./components/element/VFormSelect.vue";
+import VFormDate from "./components/element/VFormDate.vue";
 
 const virsas = {
   install(Vue) {
     Vue.component("VLogo", VLogo);
+    Vue.component("VForm", VForm);
+    Vue.component("VFormInput", VFormInput);
+    Vue.component("VFormSelect", VFormSelect);
+    Vue.component("VFormDate", VFormDate);
   },
 };
 
